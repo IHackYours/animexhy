3423cd48916f5668ab1be5d903301de14fdeb61a
IHackYours_ <73014687+IHackYours@users.noreply.github.com>
2021-01-27 11:40:36 +0700

    Update homepage.js

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Loading from "../components/Loading";
 import { Link } from "react-router-dom";
-import { AiFillGithub } from "react-icons/ai";
+import { SiWhatsapp } from "react-icons/ai";
 import countapi from "countapi-js";
 
 const Main = styled.div`
@@ -255,13 +255,13 @@ const Home = () => {
         </Div>
       </Wrap>
       <Maindiv>
-        <Git href="https://wa.me/6283140861605">
-          <AiFillGithub
+        <Wa href="https://wa.me/6283140861605">
+          <SiWhatsapp
             style={{ position: "relative", top: "10px" }}
             size="2.5em"
           />
           IHackYours
-        </Git>
+        </Wa>
       </Maindiv>
     </Main>
   );
